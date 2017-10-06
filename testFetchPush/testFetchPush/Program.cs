4ae1134cc212ffa4bf5b0c73eca78e42dadb6c10
Carmen Vargas <mobappdev1@gmail.com>
2017-10-06 12:22:00 +0200

    first commit

--- testFetchPush/testFetchPush/Program.cs
+++ testFetchPush/testFetchPush/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NewOrigenTestGitVs
+namespace testFetchPush
 {
     class Program
     {
--- testFetchPush/testFetchPush/Properties/AssemblyInfo.cs
+++ testFetchPush/testFetchPush/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NewOrigenTestGitVs")]
+[assembly: AssemblyTitle("testFetchPush")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NewOrigenTestGitVs")]
+[assembly: AssemblyProduct("testFetchPush")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7e8f9e4c-b91d-4410-b286-d9646a3b361c")]
+[assembly: Guid("7ef0376c-9761-496a-a00f-242bbc2a9cf7")]
 
 // Version information for an assembly consists of the following four values:
 //
