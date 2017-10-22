9883cb4bca405bc9e02ea3dcfeaa4285e7eb12ae
Carmen Vargas <mobappdev1@gmail.com>
2017-10-23 00:38:52 +0200

    Interfaces 2

---
+++ EserciziCSharp1/Interfaces2/Planes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces2
+{
+    class Planes : IFlyabile
+    {
+        public string Canfly()
+        {
+            return "Planes can fly";
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ EserciziCSharp1/Interfaces2/Program.cs
@@ -0,0 +1,33 @@
+﻿using Interfaces2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces2
+{
+    class Program    {
+        static void Main(string[] args)
+        {
+            List<IFlyabile> things = new List<IFlyabile>()
+            {
+                new Birds(),
+                new Planes()
+                
+            };
+           
+           
+            foreach (IFlyabile item in things)
+              {
+                //item.Canfly();
+               Console.WriteLine("item:  " + item.Canfly().ToString());
+            }
+            Console.ReadLine();
+        }
+
+        
+    }
+
+    
+}
