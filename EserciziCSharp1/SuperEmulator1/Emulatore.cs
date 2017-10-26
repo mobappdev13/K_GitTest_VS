197eea1f8aa4260e03fcaeaf883c454f88735aa5
allievo04 ires <allievo04.ires@outlook.it>
2017-10-26 09:13:24 +0200

    super emulator

---
+++ EserciziCSharp1/SuperEmulator1/Emulatore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperEmulator1
+{
+    public class Emulator
+    {
+        public string _isCallStart;
+
+       
+        public HeadSet SelectedModel { get; set; }
+        public string CallStatus
+        {
+            get { return _isCallStart ? "si" : "no"; }
+            //set;}
+        }
+
+        public bool Call(string phoneNumber)
+        {
+            //attenzione gli stattin una classe call
+            return false;
+        }
+        
+        public bool HangUpCall(string phoneNumber)
+        {
+            return false;
+        }
+
+        private bool IsModelSelected()
+        {
+            return false;
+        }
+
+        
+
+    }
+}
---
+++ EserciziCSharp1/SuperEmulator1/HeadSet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperEmulator1
+{
+    public abstract class HeadSet
+    {
+        //last added
+        IFrontCameraBehavior _frontCameraBehavior;
+        IRearCameraBehavior _rearCameraBhavior;
+
+        public bool Call(double number)
+        {
+            //or void ?
+            return true;
+        
+        }
+        public bool SendSms(string Messaggio)
+        {
+            //or void 
+            return true;
+        }
+
+        ////public abstract string DisplayModel(string ) 
+        //{
+        //}
+        public void GoTakePicture()
+        {
+            _rearCameraBhavior.TakePicture();
+           
+        }
+        public void GoTakeSelfie()
+        {
+            _frontCameraBehavior.TakeSelfie() ;
+          
+        }
+
+        //public ActionResult (...)
+        //{
+            
+        //    return true 
+        //}
+    }
+}
+
