95058386c88d338a3a72a03eb3761fd67339e6dd
Carmen Vargas <mobappdev1@gmail.com>
2017-11-03 09:52:31 +0100

    Explicit Interfaces

---
+++ EserciziCSharp1/ExplicitInterface/ClassDoubleInterface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterface
+{
+    class ClassDoubleInterface : Icontrol, ISurface
+    {
+        string Icontrol.Paint()
+        {
+            return "Paint in Icontrol interface";
+        }
+        
+
+        string ISurface.Paint()
+        {
+            return "Paint in ISurface*** interface";
+        }
+
+       public string MyOtherMethod()
+        {
+            return "********this method in class";
+        }
+        public string Paint()
+        {
+            return "my paint no interfaces";
+        }
+    }
+}
---
+++ EserciziCSharp1/ExplicitInterface/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassDoubleInterface classDoubleI = new ClassDoubleInterface();
+            // call otherMethod
+            string result = classDoubleI.MyOtherMethod();
+            Console.WriteLine($"Method result: {result}");
+
+            result = classDoubleI.Paint();
+            Console.WriteLine($"Method result: {result}");
+            //con implicit interfaces and casting
+
+            // varibile del tipo Icontrol
+            //cast redundant
+            Icontrol icontrol = (Icontrol)classDoubleI;
+            result = icontrol.Paint();
+            Console.WriteLine($"Paint Method Result: {result}");
+            ISurface isurface = classDoubleI;
+            result = isurface.Paint();
+            Console.WriteLine($"Paint Method Result: {result}");
+            Console.ReadLine();
+        }
+    }
+}
